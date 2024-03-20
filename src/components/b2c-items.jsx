0b4ea5dea757e4907ebef2776b0fb07bc5a2099c
sourabh verma <sourabhverma3105@gmail.com>
2024-03-20 11:34:45 +0530

    Updates

--- src/components/b2c-items.jsx
+++ src/components/b2c-items.jsx
@@ -10,54 +10,52 @@ const B2CItems = () => {
                 <DropdownMenuTrigger>
                     <p className={`bg-[#3552AA] text-white tracking-wide py-3 px-3 text-sm font-medium rounded-sm w-[150px]`}>B2C</p>
                 </DropdownMenuTrigger>
-                <div>
-                    <DropdownMenuContent className={`flex flex-col ms-[340px] -mt-[45px]`}>
-                        <DropdownMenuSeparator />
-                        <Tooltip>
-                            <TooltipTrigger>
-                                <DropdownMenuItem>
-                                    <p className='bg-[#3552AA] text-white tracking-wide py-3 px-3 text-sm font-medium rounded-sm w-[150px]'>Online</p>
-                                </DropdownMenuItem>
-                            </TooltipTrigger>
-                            <TooltipContent>
-                                <ProgressDetailsBox />
-                            </TooltipContent>
-                        </Tooltip>
+                <DropdownMenuContent className={`flex flex-col ms-[340px] -mt-[45px]`}>
+                    <DropdownMenuSeparator />
+                    <Tooltip>
+                        <TooltipTrigger>
+                            <DropdownMenuItem>
+                                <p className='bg-[#3552AA] text-white tracking-wide py-3 px-3 text-sm font-medium rounded-sm w-[150px]'>Online</p>
+                            </DropdownMenuItem>
+                        </TooltipTrigger>
+                        <TooltipContent>
+                            <ProgressDetailsBox />
+                        </TooltipContent>
+                    </Tooltip>
 
-                        <Tooltip>
-                            <TooltipTrigger>
-                                <DropdownMenuItem>
-                                    <p className='bg-[#3552AA] text-white tracking-wide py-3 px-3 text-sm font-medium rounded-sm w-[150px]'>Interview</p>
-                                </DropdownMenuItem>
-                            </TooltipTrigger>
-                            <TooltipContent>
-                                <ProgressDetailsBox />
-                            </TooltipContent>
-                        </Tooltip>
+                    <Tooltip>
+                        <TooltipTrigger>
+                            <DropdownMenuItem>
+                                <p className='bg-[#3552AA] text-white tracking-wide py-3 px-3 text-sm font-medium rounded-sm w-[150px]'>Interview</p>
+                            </DropdownMenuItem>
+                        </TooltipTrigger>
+                        <TooltipContent>
+                            <ProgressDetailsBox />
+                        </TooltipContent>
+                    </Tooltip>
 
-                        <Tooltip>
-                            <TooltipTrigger>
-                                <DropdownMenuItem>
-                                    <p className='bg-[#3552AA] text-white tracking-wide py-3 px-3 text-sm font-medium rounded-sm w-[150px]'>Public Data</p>
-                                </DropdownMenuItem>
-                            </TooltipTrigger>
-                            <TooltipContent>
-                                <ProgressDetailsBox />
-                            </TooltipContent>
-                        </Tooltip>
+                    <Tooltip>
+                        <TooltipTrigger>
+                            <DropdownMenuItem>
+                                <p className='bg-[#3552AA] text-white tracking-wide py-3 px-3 text-sm font-medium rounded-sm w-[150px]'>Public Data</p>
+                            </DropdownMenuItem>
+                        </TooltipTrigger>
+                        <TooltipContent>
+                            <ProgressDetailsBox />
+                        </TooltipContent>
+                    </Tooltip>
 
-                        <Tooltip>
-                            <TooltipTrigger>
-                                <DropdownMenuItem>
-                                    <p className='bg-[#3552AA] text-white tracking-wide py-3 px-3 text-sm font-medium rounded-sm w-[150px]'>Health</p>
-                                </DropdownMenuItem>
-                            </TooltipTrigger>
-                            <TooltipContent>
-                                <ProgressDetailsBox />
-                            </TooltipContent>
-                        </Tooltip>
-                    </DropdownMenuContent>
-                </div>
+                    <Tooltip>
+                        <TooltipTrigger>
+                            <DropdownMenuItem>
+                                <p className='bg-[#3552AA] text-white tracking-wide py-3 px-3 text-sm font-medium rounded-sm w-[150px]'>Health</p>
+                            </DropdownMenuItem>
+                        </TooltipTrigger>
+                        <TooltipContent>
+                            <ProgressDetailsBox />
+                        </TooltipContent>
+                    </Tooltip>
+                </DropdownMenuContent>
             </DropdownMenu>
         </>
     )
